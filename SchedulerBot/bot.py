17ccc8bd5a2b81500aafca3ecd9861e112dc0506
KhyleHickey <61008665+KhyleHickey@users.noreply.github.com>
2021-11-01 20:49:00 -0800

    Update bot.py

--- SchedulerBot/bot.py
+++ SchedulerBot/bot.py
@@ -4,6 +4,7 @@ from tinydb import TinyDB, Query
 from discord.ext import commands
 from dotenv import load_dotenv
 import discord
+import datetime
 load_dotenv() #!add 2021-11-0 "Hello world"="A test input"
 TOKEN = os.getenv('DISCORD_TOKEN')
 client=discord.Client()
@@ -15,8 +16,11 @@ async def _command(ctx,*args):
         #!add {date} {time} {event} {description}
         #db.insert({'user':ctx.author.id})
         response = ""
+        
+        dt = datetime.datetime.strptime(args[0], '%y-%m-%d %H:%M')
+        today = datetime.datetime.today().replace(second=0, microsecond=0)
 
-        db.insert({'user':ctx.author.id,'date':args[0],'time':args[1],'event':args[2],'desc':args[3]})
+        db.insert({'user':ctx.author.id,'date':dt,'time':args[1],'event':args[2],'desc':args[3]})
 
         await ctx.channel.send("Event:"+args[2]+" created")
 
